a39d854b25253f46c868e77c263f4da1dc2362d6
Igor Melo <igorcoroinha@hotmail.com>
2021-04-26 18:25:12 +0200

    OMxPSG

---
+++ OMxPSG_T-Shirt_Recognition_with_ViolaJones_and_CNN./Haar_Script_3.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 15:38:52 2021
+
+@author: igor
+"""
+
+
+####### Importing Libraries ######
+from getopt import getopt
+from sys import argv
+from os import listdir, path, makedirs, remove
+from re import sub
+from subprocess import check_call
+from time import sleep
+from shutil import rmtree, copytree, copy
+import cv2 as cv
+
+######## Defining the directories for the original images ########
+
+POS_IMG = "dataset/orig-pos"
+NEG_IMG = "dataset/orig-neg"
+
+######## Defining the directories for the training sets ########
+
+DIR_PROCESSING_DATA = "dataset"
+DIR_POS_IMAGES = "dataset/pos"
+DIR_POS_IMAGES_CROPPED = DIR_PROCESSING_DATA+"/positive-clean-cropped"
+DIR_DATA = "dataset"
+DIR_NEG_IMAGES = "dataset/neg"
+DIR_SAMPLE_IMAGES = DIR_DATA+"/positive-clean-cropped-samples"
+DIR_HAAR_DATA = DIR_DATA+"/clean-cropped-haardata"
+
+FILE_NEG_COLLECTION = DIR_NEG_IMAGES+"/"+DIR_NEG_IMAGES.split("/")[-1]+".txt"
+FILE_SAMPLE_COLLECTION = DIR_SAMPLE_IMAGES+"/"+DIR_SAMPLE_IMAGES.split("/")[-1]+".txt"
+FILE_SAMPLE_VEC = DIR_SAMPLE_IMAGES+".vec"
+
+####### Starting the script for the full training process ########
+
+if __name__=="__main__":
+    (createSamples, createVec, trainCascade) = (False, False, False)
+    opts, args = getopt(argv[1:],"svt",["createSamples","createVec","trainCascade"])
+    for opt, arg in opts:
+        if(opt in ("--createSamples","-s")):
+            createSamples = True
+        elif(opt in ("--createVec","-v")):
+            createVec = True
+        elif(opt in ("--trainCascade","-t")):
+            trainCascade = True
+    
+    ######### Resizing images and changing image colors #########
+    
+    ##### Negatives Images ######
+    def resizeNegImages():
+        
+        imagePath = listdir(NEG_IMG)
+        identify = 1
+        if not path.isdir(DIR_NEG_IMAGES):
+            makedirs(DIR_NEG_IMAGES)
+        for i in imagePath:
+            
+            i.replace(i, DIR_NEG_IMAGES + "/" + str(identify) + ".JPEG")
+            copy(NEG_IMG + "/" + i, i.replace(i, DIR_NEG_IMAGES + "/" + str(identify) + ".JPEG"))
+            img = cv.imread(DIR_NEG_IMAGES + "/" + str(identify) + ".JPEG", cv.IMREAD_GRAYSCALE)
+            resize = cv.resize(img, (100, 100))
+            cv.imwrite(DIR_NEG_IMAGES + "/" + str(identify) + ".JPEG", resize)
+            
+            identify += 1
+        return identify
+    nun_neg = resizeNegImages()
+    
+    ##### Positive Images #####
+    def resizePosImages():
+        
+        imagePath = listdir(POS_IMG)
+        identify = 1
+        
+        if not path.isdir(DIR_POS_IMAGES):
+            makedirs(DIR_POS_IMAGES)
+        for i in imagePath:
+            
+            i.replace(i, DIR_POS_IMAGES + "/" + str(identify) + ".JPEG")
+            copy(POS_IMG + "/" + i, i.replace(i, DIR_POS_IMAGES + "/" + str(identify) + ".JPEG"))
+            img = cv.imread(DIR_POS_IMAGES + "/" + str(identify) + ".JPEG", cv.IMREAD_GRAYSCALE)
+            resize = cv.resize(img, (100, 100))
+            cv.imwrite(DIR_POS_IMAGES + "/" + str(identify) + ".JPEG", resize)
+            
+            identify += 1
+        return identify
+    nun_pos = resizePosImages()
+   
+    ##### To be sure that we have the output directory #####
+    if(not path.isdir(DIR_SAMPLE_IMAGES)):
+        makedirs(DIR_SAMPLE_IMAGES)
+    
+    ##### Creating the background file ######
+    negImageCollectionFile = open("neg.txt", "w")
+    negImageFilenames = [f for f in listdir(DIR_NEG_IMAGES) if path.isfile(path.join(DIR_NEG_IMAGES,f)) and f.endswith("JPEG")]
+    for f in negImageFilenames:
+        negImageCollectionFile.write("/home/igor/Documents/Artificial Inteligence/Deep-Learning/Artificial Intelligence Formation/4 - Computer Vision/Haar Cascade/dataset/neg/" + f)
+        negImageCollectionFile.write("\n")
+    negImageCollectionFile.close()
+    
+    ##### Making the list with the positve images #####
+    posImageFilenames = [f for f in listdir(DIR_POS_IMAGES) if path.isfile(path.join(DIR_POS_IMAGES,f))]
+    
+    ##### Creating the samples ######
+    if(createSamples):
+        SampleCollectionFile = open(DIR_SAMPLE_IMAGES + "/"+ "sample.lst", "w")
+        count = 1
+        for f in posImageFilenames:
+            file = "sample-" + str(count) + ".lst"
+            check_call(["opencv_createsamples",
+                "-img", path.join(DIR_POS_IMAGES,f),
+                "-bg", 'neg.txt',
+                "-info", path.join(DIR_SAMPLE_IMAGES, file),
+                "-num", str(round(1600*0.8)),
+                "-maxxangle", "0.0",
+                "-maxyangle", "0.0",
+                "-maxzangle", "0.3",
+                "-bgcolor", "255",
+                "-bgthresh", "8",
+                "-w", "40",
+                "-h", "40"])
+            with open(DIR_SAMPLE_IMAGES + "/" + file, "r") as ss:
+                for line in ss:
+                    SampleCollectionFile.write(line)
+                    #SampleCollectionFile.write("\n")
+            count += 1
+            remove(DIR_SAMPLE_IMAGES + "/" + file)
+        SampleCollectionFile.close()
+    
+    ##### Vectorizing all sample images #####
+    if(createVec):
+         check_call(["opencv_createsamples",
+            "-info", DIR_SAMPLE_IMAGES + "/sample.lst",
+            "-vec", FILE_SAMPLE_VEC,
+            "-bg", "neg.txt",
+            "-num", str(sum(1 for line in open(DIR_SAMPLE_IMAGES + "/sample.lst", "r"))),
+            "-w", "40",
+            "-h", "40"])
+         sleep(1)
+    
+    ##### Training the Haar Cascades #####
+    if(trainCascade):
+        if(not path.isdir(DIR_HAAR_DATA)):
+            makedirs(DIR_HAAR_DATA)
+
+        check_call(["opencv_traincascade",
+            "-data", DIR_HAAR_DATA,
+            "-vec", FILE_SAMPLE_VEC,
+            "-bg", "neg.txt",
+            "-numPos", str(max(50, sum(1 for line in open(DIR_SAMPLE_IMAGES + "/sample.lst", "r")))/2),
+            #"-numPos", str(50),
+            "-numNeg", str(max(50, sum(1 for line in open(DIR_SAMPLE_IMAGES + "/sample.lst", "r")))/4),
+            "-numStages", "20",
+            "-precalcValBufSize", "1024",
+            "-precalcIdxBufSize", "1024",
+            "-featureType", "HAAR",
+            "-w", "40",
+            "-h", "40",
+            "-minHitRate", "0.995",
+            "-maxFalseAlarmRate", "0.5"])
+        sleep(1)
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
---
+++ OMxPSG_T-Shirt_Recognition_with_ViolaJones_and_CNN./OMxPSG_recog.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 13:30:34 2021
+
+@author: igor
+"""
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  9 16:07:41 2021
+
+@author: igor
+"""
+
+# Importing the library
+
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+#Loading the pre-trained cascade method for faces and eyes
+tshirt = cv2.CascadeClassifier('cascade.xml') #Object to read the pre-trained frontal face detectp
+#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') #Object to read the pre-trained eyes detector
+model = load_model("tune.h5")
+
+
+
+def detect(gray, frame):
+   tshirts =  tshirt.detectMultiScale(gray, 5.0, 120, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+
+   for (x, y, w, h) in tshirts:
+       cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+       roi_gray = gray[y:y+h, x:x+w]
+       roi_color = frame[y:y+h, x:x+w]
+       roi_gray = cv2.resize(roi_gray, (160, 160))
+       roi_gray = roi_gray.astype("float") / 255.0
+       roi_gray = img_to_array(roi_gray)
+       #roi_gray = cv2.cvtColor(roi_gray, cv2.COLOR_GRAY2RGB)
+       roi_gray = np.expand_dims(roi_gray, axis=0)  
+       roi_color = cv2.resize(roi_color, (160, 160))
+       roi_color = roi_color.astype("float") / 255.0
+       roi_gray = img_to_array(roi_color)
+       roi_color = np.expand_dims(roi_color, axis=0)
+       OM = model.predict(roi_color)
+       label = "Other" if 0.5 > OM else "OM"
+       
+       cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+   return frame
+    
+    
+
+# Detecting faces with webcam
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    
+    _, frame = video_capture.read()
+    
+    #frame = imutils.resize(frame, width=300)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frameClone = frame.copy()
+    canvas = detect(gray, frameClone)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
